f4b502893f58d82f5f47308af9716685e9888c32
Ciprien Bossut <chefchino@live.com>
2019-10-21 23:03:30 -0400

    html layout

---
+++ javascript/javascript.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCW1T_39gRCtFnvvespraWM22s7Mlz4PIE",
+    authDomain: "trains-263ab.firebaseapp.com",
+    databaseURL: "https://trains-263ab.firebaseio.com",
+    projectId: "trains-263ab",
+    storageBucket: "trains-263ab.appspot.com",
+    messagingSenderId: "813486538049",
+    appId: "1:813486538049:web:f07814f2e380638dc2f5c0",
+    measurementId: "G-23M38PBQCT"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
